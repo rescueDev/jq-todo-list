fa21938d119033f6e769ab326efec8fd51ca64dd
sa-borgia3 <sa.borgia3@gmail.com>
2021-01-16 11:44:45 +0100

    delete single task raw

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ var input = $("input");
 input.keyup(function (e) {
   if (e.keyCode == 13 && e.target.value != "") {
     var task = $('<div class="task"/>').text(e.target.value);
+    var del = task.remove();
+    //delete single task
+    task.click(function () {
+      $(this).remove();
+    });
     $(".not-completed").append(task);
     input.val("");
   }
