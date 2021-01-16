5b6a0908abf06eff9e371cafc390bf1592222b6b
sa-borgia3 <sa.borgia3@gmail.com>
2021-01-16 12:05:51 +0100

    remove task completed

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ var input = $("input");
 input.keyup(function (e) {
   if (e.keyCode == 13 && e.target.value != "") {
     var task = $('<div class="task"/>').text(e.target.value);
-    var del = $('<i class="fas fa-trash"/>');
+
+    var del = $('<i class="fas fa-trash"/>').click(function () {
+      var p = $(this).parent();
+      p.fadeOut(function () {
+        p.remove();
+      });
+    });
+
     var check = $('<i class="fas fa-check"/>');
 
     task.append(del, check);
 
-    //delete single task
-    $(".task .fa-trash").click(function () {
-      $(this).remove();
-    });
+    //delete single task});
     $(".not-completed").append(task);
     input.val("");
   }
