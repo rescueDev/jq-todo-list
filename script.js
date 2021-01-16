2e749b259ec5215f8c5fc72e2d03af38c8f8b94d
sa-borgia3 <sa.borgia3@gmail.com>
2021-01-16 11:50:24 +0100

    append icon delete and check task

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ var input = $("input");
 input.keyup(function (e) {
   if (e.keyCode == 13 && e.target.value != "") {
     var task = $('<div class="task"/>').text(e.target.value);
-    var del = task.remove();
+    var del = $('<i class="fas fa-trash"/>');
+    var check = $('<i class="fas fa-check"/>');
+
+    task.append(del, check);
+
     //delete single task
-    task.click(function () {
+    $(".task .fa-trash").click(function () {
       $(this).remove();
     });
     $(".not-completed").append(task);
